b262570627e8beb2fc2700926988d7177b099952
viperadnan-git <60497650+viperadnan-git@users.noreply.github.com>
2020-02-08 17:44:37 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -12,12 +12,12 @@ URL_REGEX = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+"
 
 
 class MirrorStatus:
-    STATUS_UPLOADING = "Uploading"
-    STATUS_DOWNLOADING = "Downloading"
-    STATUS_WAITING = "Queued"
-    STATUS_FAILED = "Failed. Cleaning download"
-    STATUS_CANCELLED = "Cancelled"
-    STATUS_ARCHIVING = "Archiving"
+    STATUS_UPLOADING = "Uploading📤"
+    STATUS_DOWNLOADING = "Downloading📥"
+    STATUS_WAITING = "Queued⏳"
+    STATUS_FAILED = "Failed. Cleaning download🧹"
+    STATUS_CANCELLED = "Cancelled❌"
+    STATUS_ARCHIVING = "Archiving📦"
 
 
 PROGRESS_MAX_SIZE = 100 // 8
